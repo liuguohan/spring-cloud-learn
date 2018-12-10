92fa645bbcc18803210b054b5298038d532b84ca
liuguohan <506373724@qq.com>
2018-12-10 16:48:50 +0800

    optimize

---
+++ sc-f-chapter12/service-turbine/src/main/java/com/ServiceTurbineApplication.java
@@ -0,0 +1,30 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
+public class ServiceTurbineApplication {
+
+    /**
+     * 访问地址 http://localhost:8764/turbine.stream
+     * @param args
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTurbineApplication.class, args);
+	}
+}
---
+++ sc-f-chapter7/config-client/src/main/java/com/ConfigClientApplication.java
@@ -0,0 +1,37 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+	
+	/**
+	 * 我们只需要发送post请求：config-client会重新读取配置文件
+	 * http://localhost:8881/actuator/bus-refresh
+	 * 另外，/actuator/bus-refresh接口可以指定服务，
+	 * 即使用"destination"参数，比如 “/actuator/bus-refresh?destination=config-client:**” 即刷新服务名为config-client的所有服务。
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+	
+	@Value("${foo}")
+	String foo;
+	
+	@RequestMapping(value = "/hi")
+	public String hi() {
+		return foo;
+	}
+	
+}
